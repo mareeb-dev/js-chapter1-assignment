a20b7a438189960584b8a96f3ddd200fb26b8d42
mareeb-dev <muhammadareeb768@gmail.com>
2024-08-10 13:22:16 +0500

    completed

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1. Write a script to greet your website visitor using JS alert box
+
+alert("Hello there! Thanks for visiting our site!");
+
+// 2. Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid password.");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// 5. Generate the following message through browser’s developer console:
+
+console.log("alert");
+alert("Hello...I can run JS through my web browser's console");
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+
+console.log("https://mareeb-dev.github.io/the-analog-specialized-assignment/");
+
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+console.log("A check in above link B scripts are not placed outside the <body> tag. C check in above link D scripts are not placed after the html page.");
